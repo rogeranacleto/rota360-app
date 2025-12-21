dcb25b5403c6bc250d9168b34d012b00a7b6da99
Roger Anacleto <rogerdasilvacode7@gmail.com>
2025-12-21 18:59:02 -0300

    feat: add history page with completed trips table

---
+++ src/pages/Admin/History/index.tsx
@@ -0,0 +1,8 @@
+import { TableHistory } from "../../../components/TableHistory"
+export function History(){
+    return(
+        <div className="bg-[#EEEEEE] max-w-screen min-h-screen rounded-2xl p-7">
+            <TableHistory/>
+        </div>
+    )
+}
